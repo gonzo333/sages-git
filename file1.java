829baf14c83bb49dbdcf70578b66b8694389bf76
gonzo333 <pawelnowaczek88@gmail.com>
2022-11-05 12:30:13 +0100

    change file1 first_branch branch

--- file1.java
+++ file1.java
@@ -2,5 +2,6 @@ public class file1 {
     public static void main(String[] args) {
         System.out.println("change first");
         System.out.println("change second");
+        System.out.println("change third from first_branch branch");
     }
 }
